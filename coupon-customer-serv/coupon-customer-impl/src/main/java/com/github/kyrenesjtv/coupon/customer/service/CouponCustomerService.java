3dc3f3238aae5278a157c9fb6cc8822d212df0dd
albert <sjtv5.12@gmail.com>
2022-05-01 23:18:29 +0800

    用户消费服务

---
+++ coupon-customer-serv/coupon-customer-impl/src/main/java/com/github/kyrenesjtv/coupon/customer/service/CouponCustomerService.java
@@ -0,0 +1,30 @@
+package com.github.kyrenesjtv.coupon.customer.service;
+
+
+import com.github.kyrenesjtv.coupon.calculation.api.beans.ShoppingCart;
+import com.github.kyrenesjtv.coupon.calculation.api.beans.SimulationOrder;
+import com.github.kyrenesjtv.coupon.calculation.api.beans.SimulationResponse;
+import com.github.kyrenesjtv.coupon.customer.api.beans.RequestCoupon;
+import com.github.kyrenesjtv.coupon.customer.api.beans.SearchCoupon;
+import com.github.kyrenesjtv.coupon.customer.dao.entity.Coupon;
+import com.github.kyrenesjtv.coupon.template.api.beans.CouponInfo;
+
+import java.util.List;
+
+// 用户对接服务
+public interface CouponCustomerService {
+
+    // 领券接口
+    Coupon requestCoupon(RequestCoupon request);
+
+    // 核销优惠券
+    ShoppingCart placeOrder(ShoppingCart info);
+
+    // 优惠券金额试算
+    SimulationResponse simulateOrderPrice(SimulationOrder order);
+
+    void deleteCoupon(Long userId, Long couponId);
+
+    // 查询用户优惠券
+    List<CouponInfo> findCoupon(SearchCoupon request);
+}
