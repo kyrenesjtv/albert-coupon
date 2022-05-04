96376cad01fae5597dfcc153ce088231608a78be
albert <sjtv5.12@gmail.com>
2022-05-04 17:47:10 +0800

    OpenFeign之降级策略。

--- coupon-customer-serv/coupon-customer-impl/src/main/java/com/github/kyrenesjtv/coupon/customer/feign/TemplateService.java
+++ coupon-customer-serv/coupon-customer-impl/src/main/java/com/github/kyrenesjtv/coupon/customer/feign/TemplateService.java
@@ -1,5 +1,6 @@
 package com.github.kyrenesjtv.coupon.customer.feign;
 
+import com.github.kyrenesjtv.coupon.customer.feign.fallback.TemplateServiceFallbackFactory;
 import com.github.kyrenesjtv.coupon.template.api.beans.CouponTemplateInfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Collection;
 import java.util.Map;
 
-@FeignClient(value = "coupon-template-serv", path = "/template")//value 服务名称 ， path restController的前缀。此接口应该是由服务提供方维护，专门抽成一个jar包
+@FeignClient(value = "coupon-template-serv", path = "/template"
+//        ,fallback = TemplateServiceFallback.class
+        ,fallbackFactory = TemplateServiceFallbackFactory.class
+        )//value 服务名称 ， path restController的前缀。此接口应该是由服务提供方维护，专门抽成一个jar包。fallback降级策略。fallbackFactory降级策略，还能输出错误信息。两者选其一
 public interface TemplateService {
 
     // 读取优惠券
---
+++ coupon-customer-serv/coupon-customer-impl/src/main/java/com/github/kyrenesjtv/coupon/customer/feign/fallback/TemplateServiceFallback.java
@@ -0,0 +1,26 @@
+package com.github.kyrenesjtv.coupon.customer.feign.fallback;
+
+import com.github.kyrenesjtv.coupon.customer.feign.TemplateService;
+import com.github.kyrenesjtv.coupon.template.api.beans.CouponTemplateInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class TemplateServiceFallback implements TemplateService {
+
+    @Override
+    public CouponTemplateInfo getTemplate(Long id) {
+        log.info("fallback getTemplate");
+        return null;
+    }
+
+    @Override
+    public Map<Long, CouponTemplateInfo> getTemplateInBatch(Collection<Long> ids) {
+        log.info("fallback getTemplateInBatch");
+        return null;
+    }
+}
